e2d9bf4d15a562985634a0f57f16d8c5670bbd41
Becho:33 <68383711+Becho33@users.noreply.github.com>
2022-04-18 11:11:16 +0100

    Add files via upload

---
+++ comsApp/calander api/serviceToBusiness.js
@@ -0,0 +1,24 @@
+var serviceNamesSelectedByclient = {car, massage, hair};
+var howFrequent = "";
+var ContactDetailsOfClient = "";
+var businessId = "";
+var businessEmail = "";
+var businessPhone = "";
+var listOfAllbusinessAndEmail = {"massage": "this@email.com", "car rental": "car@het.com"}
+
+
+for (var i = 0; i < serviceNamesSelectedByclient.length; i++) {
+    for (var j = 0; j < listOfAllbusinessAndEmail.length) {
+        if (i < serviceNamesSelectedByclient.length) {
+            function const emailbusiness = new type(listOfAllbusinessAndEmail[i]) {
+                if (serviceNamesSelectedByclient[] === listOfAllbusinessAndEmail[]) {
+                    function const sendemail = new type() {
+                        var time = "";
+                        var date = "";
+                        console.log("send email to busines with data");
+                    }
+                }
+            }
+
+    }
+}
